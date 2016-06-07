aa02e3c537ba455cb98fd279d1b9dd9daedf40e5
JabJabJab <TheMcJabbar@gmail.com>
2016-06-07 04:34:47 -0500

    Moved IRC-BOT methods to ZUtil for general use.

--- src/sledgehammer/util/ZUtil.java
+++ src/sledgehammer/util/ZUtil.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import sledgehammer.SledgeHammer;
@@ -133,6 +138,48 @@ public class ZUtil {
 		
 		// Return whether or not the result is a boolean true.
 		return admin.equalsIgnoreCase("true") || admin.equalsIgnoreCase("1");
+	}
+	
+	/**
+	 * Returns a String representation of the current time.
+	 * @return
+	 */
+	public static String getHourMinuteSeconds() {
+		String hours =  Calendar.getInstance().get(11) + "";
+		if ( Calendar.getInstance().get(11) < 10) {
+			hours = "0" + hours;
+		}
+		
+		String minutes = Calendar.getInstance().get(12) + "";
+		if (Calendar.getInstance().get(12) < 10) {
+			minutes = "0" + minutes;
+		}
+		
+		String seconds = Calendar.getInstance().get(13) + "";
+		if (Calendar.getInstance().get(13) < 10) {
+			seconds = "0" + seconds;
+		}
+		return Calendar.getInstance().get(11) + ":" + minutes + ":" + seconds;
+	}
+	
+	public static void compactList(@SuppressWarnings("rawtypes") List list) {
+		List<Integer> listIndexesToRemove = new ArrayList<>();
+		Map<Object, Boolean> cacheMap = new HashMap<>();
+		
+		for(int index = 0; index < list.size(); index++) {
+			Object o = list.get(index);
+			
+			Boolean cached = cacheMap.get(o);
+			if(cached == null) {
+				cacheMap.put(o, Boolean.valueOf(true));
+			} else {
+				listIndexesToRemove.add(index);
+			}
+		}
+		
+		synchronized(list) {
+			for(int index : listIndexesToRemove) list.remove(index);
+		}
 	}
 
 }
