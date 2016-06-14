a286c41cdd6ce9ebbf753c42951f891e4bafa1d4
JabJabJab <TheMcJabbar@gmail.com>
2016-06-14 03:44:44 -0500

    Created ActionMoveToLocation.

---
+++ src/sledgehammer/npc/ActionMoveToLocation.java
@@ -0,0 +1,15 @@
+package sledgehammer.npc;
+
+public class ActionMoveToLocation extends Action {
+
+	public static final String NAME = "Action->MoveToLocation";
+	
+	@Override
+	public void act(NPC npc) {
+		
+	}
+
+	@Override
+	public String getName() { return NAME; }
+
+}
