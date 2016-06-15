7ddb5ec279b115aeda33ab596119be8931c474c5
JabJabJab <TheMcJabbar@gmail.com>
2016-06-15 03:30:53 -0500

    Removed GroupPermissionHandler artifact.

--- src/sledgehammer/interfaces/GroupPermissionHandler.java
+++
@@ -1,7 +0,0 @@
-package sledgehammer.interfaces;
-
-import sledgehammer.wrapper.PermissionObject;
-
-public interface GroupPermissionHandler extends PermissionsHandler {
-	public PermissionObject getGroup(String username);
-}
