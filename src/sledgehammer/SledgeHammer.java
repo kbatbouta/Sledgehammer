26fac188e0067745832fef77d9addb59786cdf3f
Joshua Edwards <40BlocksUnder@gmail.com>
2016-07-05 09:31:48 -0500

    Updated to version "2.05_03"

--- src/sledgehammer/SledgeHammer.java
+++ src/sledgehammer/SledgeHammer.java
@@ -37,7 +37,7 @@ import zombie.network.ServerOptions;
  */
 public class SledgeHammer extends Printable {
 	
-	public static final String VERSION = "2.05_02";
+	public static final String VERSION = "2.05_03";
 	
 	/**
 	 * Debug boolean for the SledgeHammer engine. Used for verbose output.
