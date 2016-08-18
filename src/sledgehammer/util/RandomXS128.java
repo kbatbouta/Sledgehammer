196c68b498da76c74a8d7a93d560c610590f6ccc
Joshua Edwards <40BlocksUnder@gmail.com>
2016-08-18 01:50:31 -0500

    Fixed Copyright collision with RandomXS128.java.

--- src/sledgehammer/util/RandomXS128.java
+++ src/sledgehammer/util/RandomXS128.java
@@ -14,24 +14,16 @@
  * limitations under the License.
  ******************************************************************************/
 
+/**
+ * NOTE: This is an external library, and the source is avaliable. This is under
+ * the copyright above, not the license of Sledgehammer. 
+ * 
+ * Thanks.
+ * 
+ *  -Jab
+ */
+
 package sledgehammer.util;
-
-/*
-This file is part of Sledgehammer.
-
-   Sledgehammer is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Sledgehammer is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with Sledgehammer. If not, see <http://www.gnu.org/licenses/>.
-*/
 
 import java.util.Random;
 
