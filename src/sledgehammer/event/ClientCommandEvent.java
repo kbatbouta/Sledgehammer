9bec642ccd879063e4b387097a0dd291927aa618
joshu <joshu@DESKTOP-16RRRDQ.home>
2017-05-01 08:14:54 -0700

    Added ClientCommandEvent.

---
+++ src/sledgehammer/event/ClientCommandEvent.java
@@ -0,0 +1,52 @@
+package sledgehammer.event;
+
+import se.krka.kahlua.vm.KahluaTable;
+import sledgehammer.wrapper.Player;
+
+public class ClientCommandEvent extends PlayerEvent {
+
+	public static final String ID = "ClientCommandEvent";
+	
+	private String module;
+	private String command;
+	private KahluaTable table;
+	
+	public ClientCommandEvent(Player player, String module, String command, KahluaTable table) {
+		super(player);
+		setModule(module);
+		setCommand(command);
+		setTable(table);
+	}
+	
+	public String getModule() {
+		return this.module;
+	}
+	
+	private void setModule(String module) {
+		this.module = module;
+	}
+	
+	public String getCommand() {
+		return this.command;
+	}
+
+	private void setCommand(String command) {
+		this.command = command;
+	}
+	
+	public KahluaTable getTable() {
+		return this.table;
+	}
+	
+	private void setTable(KahluaTable table) {
+		this.table = table;
+	}
+
+	@Override
+	public String getLogMessage() {
+		return null;
+	}
+
+	@Override public String getID() { return ID; }
+
+}
