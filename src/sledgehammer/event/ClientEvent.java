83029bee58be364c1790498853974fc1078a7b8a
Jab <40BlocksUnder@gmail.com>
2017-05-13 11:24:32 -0700

    Added Module and Request support.

--- src/sledgehammer/event/ClientEvent.java
+++ src/sledgehammer/event/ClientEvent.java
@@ -10,39 +10,60 @@ public class ClientEvent extends PlayerEvent {
 	public static final String ID = "ClientCommandEvent";
 	
 	private String module;
+	private String moduleRaw;
 	private String command;
+	private boolean request = false;
 	private KahluaTable table;
 	
 	public ClientEvent(Player player, String module, String command, KahluaTable table) {
 		super(player);
-		setModule(module);
+		
+		this.moduleRaw = module;
+		
+		if(moduleRaw.startsWith("request:")) {
+			request = true;
+		}
+		
+		if(moduleRaw.startsWith("sledgehammer.module.") || moduleRaw.startsWith("request:sledgehammer.module.")) {
+			String[] split = module.split("sledgehammer.module.");
+			setModule(split[1]);
+		} else {			
+			setModule(module);
+		}
 		setCommand(command);
 		setTable(table);
 	}
 	
 	public void respond() {
-		GameServer.sendServerCommand(getModule(), getCommand(), getTable(), getPlayer().getConnection());
+		GameServer.sendServerCommand(getModuleRaw(), getCommand(), getTable(), getPlayer().getConnection());
 	}
-	
-	
+
 	public void respond(KahluaTable table) {
-		GameServer.sendServerCommand(getModule(), getCommand(), table, getPlayer().getConnection());
+		GameServer.sendServerCommand(getModuleRaw(), getCommand(), table, getPlayer().getConnection());
 	}
 	
 	public void respond(String command, KahluaTable table) {
-		GameServer.sendServerCommand(getModule(), command, table, getPlayer().getConnection());
+		GameServer.sendServerCommand(getModuleRaw(), command, table, getPlayer().getConnection());
 	}
 
 	public void respond(LuaTable obj) {
-		GameServer.sendServerCommand(getModule(), getCommand(), obj.export(), getPlayer().getConnection());
+		GameServer.sendServerCommand(getModuleRaw(), getCommand(), obj.export(), getPlayer().getConnection());
 	}
 	
 	public void respond(String command, LuaTable obj) {
-		GameServer.sendServerCommand(getModule(), command, obj.export(), getPlayer().getConnection());
+		GameServer.sendServerCommand(getModuleRaw(), command, obj.export(), getPlayer().getConnection());
+	}
+	
+	public boolean isRequest() {
+		return this.request;
 	}
 	
 	public String getModule() {
 		return this.module;
+	}
+	
+	private String getModuleRaw() {
+		return moduleRaw;
 	}
 	
 	private void setModule(String module) {
