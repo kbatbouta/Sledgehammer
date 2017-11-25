0a1e63ce73b9ee1147675ea59b0d22d2de914e14
Jab <40BlocksUnder@gmail.com>
2017-11-25 09:20:21 -0600

    Renamed 'Context' to 'Node' for clarity and contingency.

--- src/sledgehammer/interfaces/CommandListener.java
+++ src/sledgehammer/interfaces/CommandListener.java
@@ -34,5 +34,5 @@ public interface CommandListener {
 	
 	public String onTooltip(Player player, Command command);
 	
-	public String getPermissionContext(String context);
+	public String getPermissionNode(String command);
 }
