d9af6d343ee5aa75fecb33aeb8050d2d53ff03b0
Jab <40BlocksUnder@gmail.com>
2017-11-26 03:33:01 -0600

    Added Mongo Transaction class for outgoing transactions.

---
+++ src/sledgehammer/database/transaction/MongoDocumentTransaction.java
@@ -0,0 +1,22 @@
+package sledgehammer.database.transaction;
+
+import sledgehammer.database.MongoCollection;
+
+public abstract class MongoDocumentTransaction {
+	
+	private MongoCollection mongoCollection;
+	
+	public MongoDocumentTransaction(MongoCollection mongoCollection) {
+		setMongoCollection(mongoCollection);
+	}
+	
+	public MongoCollection getMongoCollection() {
+		return this.mongoCollection;
+	}
+
+	private void setMongoCollection(MongoCollection mongoCollection) {
+		this.mongoCollection = mongoCollection;
+	}
+	
+	public abstract void run();
+}
