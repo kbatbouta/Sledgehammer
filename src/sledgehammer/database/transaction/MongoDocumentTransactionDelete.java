f9097dbc1ab931d79af2ff917f659130a6ee70d3
Jab <40BlocksUnder@gmail.com>
2017-11-26 04:14:40 -0600

    Added Mongo Transaction classes.

---
+++ src/sledgehammer/database/transaction/MongoDocumentTransactionDelete.java
@@ -0,0 +1,47 @@
+package sledgehammer.database.transaction;
+
+import com.mongodb.BasicDBObject;
+
+import sledgehammer.database.MongoCollection;
+import sledgehammer.database.MongoDatabase;
+
+public class MongoDocumentTransactionDelete extends MongoDocumentTransaction {
+	
+	private String field;
+	private Object value;
+	
+	public MongoDocumentTransactionDelete(MongoCollection collection, String field, Object value) {
+		super(collection);
+		setField(field);
+		setValue(value);
+	}
+	
+	@Override
+	public void run() {
+		String field = getField();
+		Object value = getValue();
+		
+		if(MongoDatabase.DEBUG) {
+			System.out.println("===> Deleting document in Collection: \"" + getMongoCollection().getDBCollection().getName() + "\".");
+			System.out.println("Document ID: " + field + " Value: " + value);
+		}
+		getMongoCollection().getDBCollection().remove(new BasicDBObject(field, value));
+	}
+
+	public String getField() {
+		return this.field;
+	}
+	
+	private void setField(String field) {
+		this.field = field;
+	}
+
+	public Object getValue() {
+		return this.value;
+	}
+	
+	private void setValue(Object value) {
+		this.value = value;
+	}
+
+}
---
+++ src/sledgehammer/database/transaction/MongoDocumentTransactionUpsert.java
@@ -0,0 +1,58 @@
+package sledgehammer.database.transaction;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+import sledgehammer.database.MongoCollection;
+import sledgehammer.database.MongoDatabase;
+
+public class MongoDocumentTransactionUpsert extends MongoDocumentTransaction {
+
+	private DBObject object;
+	private String field;
+	private Object lock;
+
+	public MongoDocumentTransactionUpsert(MongoCollection collection, DBObject object, String field, Object lock) {
+		super(collection);
+		setObject(object);
+		setField(field);
+		setLock(lock);
+	}
+
+	@Override
+	public void run() {
+		String field = getField();
+		DBObject object = getObject();
+		if(MongoDatabase.DEBUG) {
+			System.out.println("===> Upserting document in Collection: \"" + getMongoCollection().getDBCollection().getName() + "\".");
+			System.out.println("Document ID: " + field + " Value: " + object.get(field));
+		}
+		BasicDBObject append = new BasicDBObject();
+		append.append("$set", object);
+		getMongoCollection().getDBCollection().update(new BasicDBObject(field, object.get(field)), append, true, false);
+	}
+
+	public Object getLock() {
+		return this.lock;
+	}
+
+	private void setLock(Object lock) {
+		this.lock = lock;
+	}
+
+	public DBObject getObject() {
+		return this.object;
+	}
+
+	private void setObject(DBObject object) {
+		this.object = object;
+	}
+
+	public String getField() {
+		return this.field;
+	}
+
+	private void setField(String field) {
+		this.field = field;
+	}
+}
