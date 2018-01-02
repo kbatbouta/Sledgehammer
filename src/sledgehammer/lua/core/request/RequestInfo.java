a53b849cf51217ab9ede2f551716a91d3b406f70
Jab <40BlocksUnder@gmail.com>
2018-01-02 15:58:52 -0600

    Documented RequestInfo.

--- src/sledgehammer/lua/core/request/RequestInfo.java
+++ src/sledgehammer/lua/core/request/RequestInfo.java
@@ -16,52 +16,53 @@ This file is part of Sledgehammer.
  */
 package sledgehammer.lua.core.request;
 
-import java.util.Map;
-
 import se.krka.kahlua.vm.KahluaTable;
 import sledgehammer.lua.LuaTable;
 import sledgehammer.lua.core.Player;
 
 /**
- * TODO: Document.
+ * LuaTable that handles the <ModuleCore> client-request for the <Player>'s
+ * information.
  * 
  * @author Jab
  */
 public class RequestInfo extends LuaTable {
 
-	private int playerID = -1;
+	/** The <Player> Object of the Player requesting information. */
 	private Player self;
 
+	/**
+	 * Main constructor.
+	 */
 	public RequestInfo() {
 		super("requestInfo");
 	}
 
-	public void setPlayerID(int id) {
-		if (this.playerID != id) {
-			this.playerID = id;
-			set("playerID", this.playerID);
-		}
-	}
-
-	public void construct(Map<Object, Object> definitions) {
-		definitions.put("self", getSelf());
-	}
-
+	@Override
 	public void onLoad(KahluaTable table) {
-		// Server authored only.
-	}
-
-	public Player getSelf() {
-		return this.self;
-	}
-
-	public void setSelf(Player player) {
-		this.self = player;
-		set("self", getSelf());
+		// (Note: Players will only be authored by the server.)
+		throw new IllegalStateException("RequestInfo objects cannot be loaded from Lua.");
 	}
 
 	@Override
 	public void onExport() {
 		set("self", getSelf());
+	}
+
+	/**
+	 * @return Returns the <Player> Object of the Player requesting information.
+	 */
+	public Player getSelf() {
+		return this.self;
+	}
+
+	/**
+	 * Sets the <Player> Object of the Player requesting information.
+	 * 
+	 * @param player
+	 *            The <Player> Object to set.
+	 */
+	public void setSelf(Player player) {
+		this.self = player;
 	}
 }
