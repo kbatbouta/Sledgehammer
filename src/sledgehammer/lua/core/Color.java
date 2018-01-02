589e2ad0b784eec42ae41ba75973b6ba08373cdc
Jab <40BlocksUnder@gmail.com>
2018-01-02 14:11:44 -0600

    Documented Color.

--- src/sledgehammer/lua/core/Color.java
+++ src/sledgehammer/lua/core/Color.java
@@ -23,18 +23,12 @@ import se.krka.kahlua.vm.KahluaTable;
 import sledgehammer.lua.LuaTable;
 
 /**
- * Color Lua class for SledgeHammer Lua.
+ * LuaTable that handles the storage of color data, and color utilities for the
+ * Sledgehammer engine.
  *
- * TODO: Document
- * 
  * @author Jab
  */
 public class Color extends LuaTable implements Comparable<Color> {
-
-	/**
-	 * Name of the LuaObject.
-	 */
-	public static final String NAME = "Color";
 
 	// @formatter:off
 	public static final Color WHITE        = new Color(1.0f , 1.0f , 1.0f );
@@ -66,97 +60,143 @@ public class Color extends LuaTable implements Comparable<Color> {
 	public static final Color GOLD         = new Color(1.0F , 0.84F, 0.0F );
 	// @formatter:on
 
+	/** The <Map> storing <String> keys, paired with the <Color> constants. */
 	public static Map<String, Color> mapColors;
-	static {
-		mapColors = new HashMap<>();
-		// @formatter:off
-		mapColors.put("white"       , WHITE       );
-		mapColors.put("light-gray"  , LIGHT_GRAY  );
-		mapColors.put("dark-gray"   , DARK_GRAY   );
-		mapColors.put("black"       , BLACK       );
-		mapColors.put("light-red"   , LIGHT_RED   );
-		mapColors.put("red"         , RED         );
-		mapColors.put("dark-red"    , DARK_RED    );
-		mapColors.put("beige"       , BEIGE       );
-		mapColors.put("orange"      , ORANGE      );
-		mapColors.put("brown"       , BROWN       );
-		mapColors.put("light-yellow", LIGHT_YELLOW);
-		mapColors.put("yellow"      , YELLOW      );
-		mapColors.put("dark-yellow" , DARK_YELLOW );
-		mapColors.put("light-green" , LIGHT_GREEN );
-		mapColors.put("green"       , GREEN       );
-		mapColors.put("dark-green"  , DARK_GREEN  );
-		mapColors.put("indigo"      , INDIGO      );
-		mapColors.put("light-blue"  , LIGHT_BLUE  );
-		mapColors.put("blue"        , BLUE        );
-		mapColors.put("dark-blue"   , DARK_BLUE   );
-		mapColors.put("light-purple", LIGHT_PURPLE);
-		mapColors.put("purple"      , PURPLE      );
-		mapColors.put("dark-purple" , DARK_PURPLE );
-		mapColors.put("pink"        , PINK        );
-		mapColors.put("dark-aqua"   , DARK_AQUA   );
-		mapColors.put("aqua"        , AQUA        );
-		mapColors.put("gold"        , GOLD        );
-		// @formatter:on
-	}
 
-	public static Color getColor(String color) {
-		return mapColors.get(color.toLowerCase());
-	}
-
-	/**
-	 * Red (0.0-1.0 Float) (Default is 0.0F)
-	 */
+	/** Red (0.0-1.0 Float) (Default is 0.0F) */
 	private float red = 0.0F;
-
-	/**
-	 * Green (0.0-1.0 Float) (Default is 0.0F)
-	 */
+	/** Green (0.0-1.0 Float) (Default is 0.0F) */
 	private float green = 0.0F;
-
-	/**
-	 * Blue (0.0-1.0 Float) (Default is 0.0F)
-	 */
+	/** Blue (0.0-1.0 Float) (Default is 0.0F) */
 	private float blue = 0.0F;
-
-	/**
-	 * Alpha (0.0-1.0 Float) (Default is 1.0F)
-	 */
+	/** Alpha (0.0-1.0 Float) (Default is 1.0F) */
 	private float alpha = 1.0F;
 
 	/**
-	 * Main Constructor
+	 * New constructor.
+	 * 
+	 * @param r
+	 *            The <Float> red value, between 0 and 1.
+	 * @param g
+	 *            The <Float> green value, between 0 and 1.
+	 * @param b
+	 *            The <Float> blue value, between 0 and 1.
 	 */
-	public Color() {
-		super(NAME);
+	public Color(float r, float g, float b) {
+		super("Color");
+		setRed(r);
+		setGreen(g);
+		setBlue(b);
 	}
 
+	/**
+	 * New constructor with alpha option.
+	 * 
+	 * @param r
+	 *            The <Float> red value, between 0 and 1.
+	 * @param g
+	 *            The <Float> green value, between 0 and 1.
+	 * @param b
+	 *            The <Float> blue value, between 0 and 1.
+	 * @param a
+	 *            The <Float> alpha value, between 0 and 1.
+	 */
 	public Color(float r, float g, float b, float a) {
-		super(NAME);
+		super("Color");
 		setRed(r);
 		setGreen(g);
 		setBlue(b);
 		setAlpha(a);
 	}
 
-	public Color(float r, float g, float b) {
-		super(NAME);
-		setRed(r);
-		setGreen(g);
-		setBlue(b);
-	}
-
+	/**
+	 * KahLua load constructor.
+	 * 
+	 * @param table
+	 *            The <KahluaTable> storing the <Color> data.
+	 */
 	public Color(KahluaTable table) {
-		super(NAME, table);
+		super("Color", table);
 	}
 
 	/**
-	 * Sets all color values, and the Alpha value.
+	 * Empty Constructor. The default color is Black, being that red will be 0,
+	 * green will be 0, blue will be 0, and alpha will be 1.
+	 */
+	public Color() {
+		super("Color");
+	}
+
+	/**
+	 * Returns 0 if the other color matches. Returns 1 if not matching. Returns 2 if
+	 * the colors match, however the alpha do not.
+	 * 
+	 * @param other
+	 *            The <Color> to compare.
+	 * @return Returns the standard <Integer> result from <Comparator>'s.
+	 */
+	@Override
+	public int compareTo(Color other) {
+		// Variables
+		float r, g, b, a, or, og, ob, oa;
+		// This color.
+		r = getRed();
+		g = getGreen();
+		b = getBlue();
+		a = getAlpha();
+		// Other color
+		or = other.getRed();
+		og = other.getGreen();
+		ob = other.getBlue();
+		oa = other.getAlpha();
+		// Matching.
+		if (r == or && g == og && b == ob && a == oa) {
+			return 0;
+		}
+		// Matching color, but not alpha.
+		else if (r == or && g == og && b == ob && a == oa) {
+			return 2;
+		}
+		// Not matching.
+		else {
+			return 1;
+		}
+	}
+
+	@Override
+	public void onLoad(KahluaTable table) {
+		setRed((float) table.rawget("r"));
+		setGreen((float) table.rawget("g"));
+		setBlue((float) table.rawget("b"));
+		setAlpha((float) table.rawget("a"));
+	}
+
+	@Override
+	public void onExport() {
+		// @formatter:off
+		set("r", getRed()  );
+		set("g", getGreen());
+		set("b", getBlue() );
+		set("a", getAlpha());
+		// @formatter:on
+	}
+
+	@Override
+	public String toString() {
+		return getRed() + ";" + getGreen() + ";" + getBlue() + ";" + getAlpha();
+	}
+
+	/**
+	 * Sets all <Color> values, and the Alpha value.
 	 * 
 	 * @param red
+	 *            The <Float> red value, between 0 and 1.
 	 * @param green
+	 *            The <Float> green value, between 0 and 1.
 	 * @param blue
+	 *            The <Float> blue value, between 0 and 1.
 	 * @param alpha
+	 *            The <Float> alpha value, between 0 and 1.
 	 */
 	public void set(float red, float green, float blue, float alpha) {
 		// Set all color values.
@@ -170,8 +210,11 @@ public class Color extends LuaTable implements Comparable<Color> {
 	 * Sets all color values.
 	 * 
 	 * @param red
+	 *            The <Float> red value, between 0 and 1.
 	 * @param green
+	 *            The <Float> green value, between 0 and 1.
 	 * @param blue
+	 *            The <Float> blue value, between 0 and 1.
 	 */
 	public void set(float red, float green, float blue) {
 		// Set all color values.
@@ -257,10 +300,19 @@ public class Color extends LuaTable implements Comparable<Color> {
 	}
 
 	/**
+	 * @return Returns a <String> encoded color tag for Lua.
+	 */
+	public String toTag() {
+		return "<RGB:" + getRed() + "," + getGreen() + "," + getBlue() + ">";
+	}
+
+	/**
 	 * Limits a float value between 0 and 1.
 	 * 
 	 * @param value
-	 * @return
+	 *            The <Float> value to limit. If the value is lower than 0, 0 will
+	 *            be returned. if the value is larger than 1, then 1 is returned.
+	 * @return Returns a value between or equal to 0 or 1.
 	 */
 	public static float limit(float value) {
 		return limit(value, 0.0F, 1.0F);
@@ -270,74 +322,63 @@ public class Color extends LuaTable implements Comparable<Color> {
 	 * Limits a float value between a given minimum value, and maximum value.
 	 * 
 	 * @param value
+	 *            The <Float> value to limit. If the value is lower than the <Float>
+	 *            minimum value, the minimum value will be returned. if the value is
+	 *            larger than the <Float> maximum value, then the maximum value is
+	 *            returned.
 	 * @param minimum
+	 *            The <Float> minimum value for the returned Float value to be
+	 *            returned.
 	 * @param maximum
-	 * @return
+	 *            The <Float> maximum value for the returned Float value to be
+	 *            returned.
+	 * @return Returns the processed <Float> value between the Float minimum and
+	 *         Float maximum value given.
 	 */
 	public static float limit(float value, float minimum, float maximum) {
-		float f = value;
-		if (f > maximum)
-			f = maximum;
-		else if (f < minimum)
-			f = minimum;
-		return f;
+		return value > maximum ? maximum : value < minimum ? minimum : value;
 	}
 
-	@Override
-	public void onLoad(KahluaTable table) {
-		setRed((float) table.rawget("r"));
-		setGreen((float) table.rawget("g"));
-		setBlue((float) table.rawget("b"));
-		setAlpha((float) table.rawget("a"));
-	}
-
-	@Override
-	public String toString() {
-		return getRed() + ";" + getGreen() + ";" + getBlue() + ";" + getAlpha();
-	}
-
-	@Override
 	/**
-	 * Returns 0 if the other color matches. Returns 1 if not matching. Returns 2 if
-	 * the colors match, however the alpha do not.
+	 * @param color
+	 *            The <String> name of the color.
+	 * @return Returns a <Color> Object with the <String> name. If no Color is
+	 *         registered with the String name given, null is returned.
 	 */
-	public int compareTo(Color other) {
-		// Variables
-		float r, g, b, a, or, og, ob, oa;
-		// This color.
-		r = getRed();
-		g = getGreen();
-		b = getBlue();
-		a = getAlpha();
-		// Other color
-		or = other.getRed();
-		og = other.getGreen();
-		ob = other.getBlue();
-		oa = other.getAlpha();
-		// Matching.
-		if (r == or && g == og && b == ob && a == oa) {
-			return 0;
-		}
-		// Matching color, but not alpha.
-		else if (r == or && g == og && b == ob && a == oa) {
-			return 2;
-		}
-		// Not matching.
-		else {
-			return 1;
-		}
+	public static Color getColor(String color) {
+		return mapColors.get(color.toLowerCase());
 	}
 
-	@Override
-	public void onExport() {
-		set("r", getRed());
-		set("g", getGreen());
-		set("b", getBlue());
-		set("a", getAlpha());
+	static {
+		mapColors = new HashMap<>();
+		// @formatter:off
+		mapColors.put("white"       , WHITE       );
+		mapColors.put("light-gray"  , LIGHT_GRAY  );
+		mapColors.put("dark-gray"   , DARK_GRAY   );
+		mapColors.put("black"       , BLACK       );
+		mapColors.put("light-red"   , LIGHT_RED   );
+		mapColors.put("red"         , RED         );
+		mapColors.put("dark-red"    , DARK_RED    );
+		mapColors.put("beige"       , BEIGE       );
+		mapColors.put("orange"      , ORANGE      );
+		mapColors.put("brown"       , BROWN       );
+		mapColors.put("light-yellow", LIGHT_YELLOW);
+		mapColors.put("yellow"      , YELLOW      );
+		mapColors.put("dark-yellow" , DARK_YELLOW );
+		mapColors.put("light-green" , LIGHT_GREEN );
+		mapColors.put("green"       , GREEN       );
+		mapColors.put("dark-green"  , DARK_GREEN  );
+		mapColors.put("indigo"      , INDIGO      );
+		mapColors.put("light-blue"  , LIGHT_BLUE  );
+		mapColors.put("blue"        , BLUE        );
+		mapColors.put("dark-blue"   , DARK_BLUE   );
+		mapColors.put("light-purple", LIGHT_PURPLE);
+		mapColors.put("purple"      , PURPLE      );
+		mapColors.put("dark-purple" , DARK_PURPLE );
+		mapColors.put("pink"        , PINK        );
+		mapColors.put("dark-aqua"   , DARK_AQUA   );
+		mapColors.put("aqua"        , AQUA        );
+		mapColors.put("gold"        , GOLD        );
+		// @formatter:on
 	}
-
-	public String toTag() {
-		return "<RGB:" + getRed() + "," + getGreen() + "," + getBlue() + ">";
-	}
-
 }
