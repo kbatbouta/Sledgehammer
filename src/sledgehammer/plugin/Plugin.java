b18b4c9c2482768c4ba488cbe2803207e92115da
Jab <40BlocksUnder@gmail.com>
2018-01-02 00:22:02 -0600

    Documented Plugin.

--- src/sledgehammer/plugin/Plugin.java
+++ src/sledgehammer/plugin/Plugin.java
@@ -37,46 +37,82 @@ import sledgehammer.SledgeHammer;
 import sledgehammer.util.Printable;
 
 /**
- * TODO: Document.
+ * Class to handle plug-in operations and utilities.
  * 
  * @author Jab
  */
 public class Plugin extends Printable {
 
+	/** Debug flag for debugging the Plugin class. */
 	private static final boolean DEBUG = false;
 
+	/** The Map storing the Modules by name. */
 	private Map<String, Module> mapModules;
+	/** The <List> of Modules to load. */
 	private List<Module> listModulesToLoad;
+	/** The <List> of Modules already loaded. */
 	private List<Module> listModulesLoaded;
+	/** The <List> of Modules to start. */
 	private List<Module> listModulesToStart;
+	/** The <List> of Modules already started. */
 	private List<Module> listModulesStarted;
+	/** The <List> of Modules that are stopped. */
 	private List<Module> listModulesStopped;
+	/** The <List> of Modules that are unloaded. */
 	private List<Module> listModulesUnloaded;
+	/** The <File> Object of the Jar File containing the plug-in. */
 	private File jarFile;
+	/**
+	 * The <ClassLoader> for the plug-in. This is the System ClassLoader for the
+	 * core plug-in.
+	 */
 	private ClassLoader classLoader;
+	/** The plug-in properties and definitions. */
 	private PluginProperties properties;
+	/**
+	 * Flag to load the classes in the plug-in. This is false for the core plug-in.
+	 */
 	private boolean loadClasses = true;
 
+	/**
+	 * Main constructor.
+	 * 
+	 * @param file
+	 *            The <File> Object of the Jar File containing the plug-in.
+	 */
 	public Plugin(File file) {
 		setJarFile(file);
 		mapModules = new HashMap<>();
-		listModulesToLoad = new LinkedList<>();
-		listModulesLoaded = new LinkedList<>();
-		listModulesToStart = new LinkedList<>();
-		listModulesStarted = new LinkedList<>();
-		listModulesStopped = new LinkedList<>();
+		// @formatter:off
+		listModulesToLoad   = new LinkedList<>();
+		listModulesLoaded   = new LinkedList<>();
+		listModulesToStart  = new LinkedList<>();
+		listModulesStarted  = new LinkedList<>();
+		listModulesStopped  = new LinkedList<>();
 		listModulesUnloaded = new LinkedList<>();
+		// @formatter:on
 	}
 
+	/**
+	 * Manual constructor.
+	 * 
+	 * @param file
+	 *            The <File> Object of the Jar File containing the plug-in.
+	 * @param properties
+	 *            The plug-in properties to set manually.
+	 */
 	public Plugin(File file, PluginProperties properties) {
 		setJarFile(file);
 		setProperties(properties);
 		mapModules = new HashMap<>();
-		listModulesToLoad = new LinkedList<>();
-		listModulesLoaded = new LinkedList<>();
-		listModulesToStart = new LinkedList<>();
-		listModulesStarted = new LinkedList<>();
-		listModulesStopped = new LinkedList<>();
+		// @formatter:off
+		listModulesToLoad   = new LinkedList<>();
+		listModulesLoaded   = new LinkedList<>();
+		listModulesToStart  = new LinkedList<>();
+		listModulesStarted  = new LinkedList<>();
+		listModulesStopped  = new LinkedList<>();
+		listModulesUnloaded = new LinkedList<>();
+		// @formatter:on
 	}
 
 	@Override
@@ -84,11 +120,17 @@ public class Plugin extends Printable {
 		return "Plugin (" + getPluginName() + ")";
 	}
 
+	/**
+	 * Loads the plug-in.
+	 */
 	public void load() {
 		loadProperties();
 		instantiateModules();
 	}
 
+	/**
+	 * Reloads the plug-in.
+	 */
 	public void reload() {
 		if (mapModules.size() > 0) {
 			if (listModulesStarted.size() > 0) {
@@ -101,6 +143,11 @@ public class Plugin extends Printable {
 		load();
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Loads the plug-in's properties and definitions.
+	 */
 	private void loadProperties() {
 		try {
 			InputStream inputStream = getStream(getJarFile(), "plugin.yml");
@@ -111,6 +158,10 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Constructs the Java Object instances of the <Module>'s assigned to the
+	 * plug-in.
+	 */
 	public void instantiateModules() {
 		try {
 			File fileJar = getJarFile();
@@ -127,6 +178,13 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Constructs the Java Object instance of the given <Module>.
+	 * 
+	 * @param moduleName
+	 *            The <String> name of the <Module> to load.
+	 * @return Returns the constructed instance of the <Module>.
+	 */
 	public Module instantiateModule(String moduleName) {
 		Module module = null;
 		ModuleProperties moduleProperties = getProperties().getModuleProperties(moduleName);
@@ -148,6 +206,9 @@ public class Plugin extends Printable {
 		return module;
 	}
 
+	/**
+	 * Processes 'onLoad()' for the plug-in's <Module>'s.
+	 */
 	public void loadModules() {
 		synchronized (this) {
 			for (Module module : listModulesToLoad) {
@@ -160,6 +221,9 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Starts the plug-in's <Module>'s.
+	 */
 	public void startModules() {
 		synchronized (this) {
 			for (Module module : listModulesToStart) {
@@ -171,6 +235,12 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Updates the plug-in's <Module>'s.
+	 * 
+	 * @param delta
+	 *            The time in milliseconds since the last tick.
+	 */
 	public void updateModules(long delta) {
 		synchronized (this) {
 			if (listModulesToLoad.size() > 0) {
@@ -213,6 +283,9 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Stops the plug-in's <Module>'s.
+	 */
 	public void stopModules() {
 		synchronized (this) {
 			for (Module module : listModulesStarted) {
@@ -224,6 +297,9 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Unloads the plug-in's <Module>'s.
+	 */
 	public void unloadModules() {
 		synchronized (this) {
 			for (Module module : listModulesLoaded) {
@@ -236,6 +312,15 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Attempts to load a given <Module>.
+	 * 
+	 * @param module
+	 *            The <Module> to load.
+	 * @return Returns true if the <Module> given loads successfully.
+	 */
 	private boolean loadModule(Module module) {
 		if (module == null) {
 			throw new IllegalArgumentException("Module given is null.");
@@ -259,6 +344,15 @@ public class Plugin extends Printable {
 		return false;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Attempts to start a given <Module>.
+	 * 
+	 * @param module
+	 *            The <Module> to start.
+	 * @return Returns true if the <Module> given starts successfully.
+	 */
 	private boolean startModule(Module module) {
 		if (module == null) {
 			throw new IllegalArgumentException("Module given is null.");
@@ -285,6 +379,15 @@ public class Plugin extends Printable {
 		return false;
 	}
 
+	/**
+	 * Updates a <Module>.
+	 * 
+	 * @param module
+	 *            The <Module> to update.
+	 * @param delta
+	 *            The time in milliseconds since the last update tick.
+	 * @return Returns true if the <Module> given updates successfully.
+	 */
 	public boolean updateModule(Module module, long delta) {
 		try {
 			module.updateModule(delta);
@@ -299,6 +402,12 @@ public class Plugin extends Printable {
 		return false;
 	}
 
+	/**
+	 * Stops a <Module>.
+	 * 
+	 * @param module
+	 *            The <Module> to stop.
+	 */
 	private void stopModule(Module module) {
 		if (module == null) {
 			throw new IllegalArgumentException("Module given is null.");
@@ -323,6 +432,12 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Unloads a <Module>.
+	 * 
+	 * @param module
+	 *            The <Module> to unload.
+	 */
 	public void unloadModule(Module module) {
 		if (module == null) {
 			throw new IllegalArgumentException("Module given is null.");
@@ -343,6 +458,13 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Adds a <Module> to the plug-in. If the Module is already loaded and added in,
+	 * IllegalArgumentException is thrown.
+	 * 
+	 * @param module
+	 *            The <Module> to add to the plug-in.
+	 */
 	public void addModule(Module module) {
 		String classLiteral = getClassLiteral(module.getClass());
 		for (Module moduleNext : listModulesToLoad) {
@@ -355,10 +477,23 @@ public class Plugin extends Printable {
 		listModulesToLoad.add(module);
 	}
 
+	/**
+	 * Removes a <Module> from the plug-in.
+	 * 
+	 * @param module
+	 *            The <Module> to remove.
+	 * @return Returns true if the <Module> is in the plug-in and is removed.
+	 */
 	public boolean removeModule(Module module) {
 		return mapModules.remove(module.getModuleName(), module);
 	}
 
+	/**
+	 * @param name
+	 *            The <String> name of the <Module>.
+	 * @return Returns the <Module> with the given <String> name. If no <Module> in
+	 *         the plug-in has the name given, null is returned.
+	 */
 	public Module getModule(String name) {
 		Module returned = null;
 		for (String moduleName : mapModules.keySet()) {
@@ -370,6 +505,12 @@ public class Plugin extends Printable {
 		return returned;
 	}
 
+	/**
+	 * @param clazz
+	 *            The Java Class of the <Module>.
+	 * @return Returns the <Module> instance of the Java Class given. If no Module
+	 *         loaded in the plug-in is of the Class given, null is returned.
+	 */
 	@SuppressWarnings("unchecked")
 	public <T extends Module> T getModule(Class<? extends Module> clazz) {
 		Module returned = null;
@@ -382,6 +523,13 @@ public class Plugin extends Printable {
 		return (T) returned;
 	}
 
+	/**
+	 * Set the plug-in to load the classes in the Jar File. This is an option for
+	 * embedded plug-ins.
+	 * 
+	 * @param flag
+	 *            The Flag to set.
+	 */
 	public void setLoadClasses(boolean flag) {
 		loadClasses = flag;
 		if (!loadClasses) {
@@ -389,74 +537,170 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * @return Returns a <Collection> of the <Module>'s registered and loaded in the
+	 *         plug-in.
+	 */
 	public Collection<Module> getModules() {
 		return this.mapModules.values();
 	}
 
+	/**
+	 * Saves a resource from the Jar File to the destination path.
+	 * 
+	 * @param jarPath
+	 *            The <String> path in the Jar File.
+	 * @param destPath
+	 *            The <String> path to save to.
+	 */
 	public void saveResourceAs(String jarPath, String destPath) {
 		saveResourceAs(jarPath, new File(destPath));
 	}
 
+	/**
+	 * Saves a resource from the Jar File to the destination path.
+	 * 
+	 * @param jarPath
+	 *            The <String> path in the Jar File.
+	 * @param dest
+	 *            The <File> destination to save to.
+	 */
 	public void saveResourceAs(String jarPath, File dest) {
 		write(getJarFile(), jarPath, dest);
 	}
 
+	/**
+	 * @return Returns the properties and definitions of the plug-in.
+	 */
 	public PluginProperties getProperties() {
 		return this.properties;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Sets the properties and definitions of the plug-in.
+	 * 
+	 * @param properties
+	 *            The <PluginProperties> to set.
+	 */
 	private void setProperties(PluginProperties properties) {
 		this.properties = properties;
 	}
 
+	/**
+	 * @return Returns the <File> Object of the Jar File.
+	 */
 	public File getJarFile() {
 		return this.jarFile;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Sets the <File> Object of the Jar File.
+	 * 
+	 * @param file
+	 *            The <File> to set.
+	 */
 	private void setJarFile(File file) {
 		this.jarFile = file;
 	}
 
+	/**
+	 * @return Returns the defined Java ClassLoader used by the plug-in.
+	 */
 	private ClassLoader getClassLoader() {
 		return this.classLoader;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Sets the defined Java ClassLoader used by the plug-in.
+	 * 
+	 * @param classLoader
+	 */
 	private void setClassLoader(ClassLoader classLoader) {
 		this.classLoader = classLoader;
 	}
 
+	/**
+	 * @return Returns the <String> name of the plug-in.
+	 */
 	public String getPluginName() {
 		return getProperties().getPluginName();
 	}
 
+	/**
+	 * @return Returns the <String> version of the plug-in.
+	 */
 	public String getPluginVersion() {
 		return getProperties().getPluginVersion();
 	}
 
+	/**
+	 * @return Returns the <String> description of the plug-in.
+	 */
 	public String getPluginDescription() {
 		return getProperties().getPluginDescription();
 	}
 
+	/**
+	 * @return Returns a <List> of the loaded <Module>'s in the plug-in.
+	 */
 	private List<Module> getLoadedModules() {
 		return listModulesLoaded;
 	}
 
+	/**
+	 * @return Returns a <List> of started <Module>'s in the plug-in.
+	 */
 	private List<Module> getStartedModules() {
 		return listModulesStarted;
 	}
 
+	/**
+	 * @return Returns whether or not the plug-in uses its own ClassLoader.
+	 */
 	public boolean loadClasses() {
 		return this.loadClasses;
 	}
 
+	/**
+	 * Returns the literal <String> form of a Java Class.
+	 * 
+	 * @param clazz
+	 *            The <Class> to translate.
+	 * @return Returns a <String> notation of the given <Class>'s location.
+	 */
 	public static String getClassLiteral(Class<?> clazz) {
 		return clazz.getPackage().getName() + "." + clazz.getSimpleName();
 	}
 
+	/**
+	 * @param jar
+	 *            The <File> Object of the Jar File.
+	 * @param source
+	 *            The <String> source path of the <File> to stream.
+	 * @return Returns a <InputStream> for a registered <File> in the given Jar
+	 *         File.
+	 * @throws IOException
+	 */
 	private static InputStream getStream(File jar, String source) throws IOException {
 		return new URL("jar:file:" + jar.getAbsolutePath() + "!/" + source).openStream();
 	}
 
+	/**
+	 * Writes a Jar File entry to a File.
+	 * 
+	 * @param jar
+	 *            The <File> Object of the Jar File.
+	 * @param source
+	 *            The <String> source path to the File entry in the Jar File.
+	 * @param destination
+	 *            The <File> destination to write the stream to.
+	 */
 	public static void write(File jar, String source, File destination) {
 		try {
 			InputStream is = getStream(jar, source);
@@ -474,6 +718,14 @@ public class Plugin extends Printable {
 		}
 	}
 
+	/**
+	 * Loads the ClassLoader of the given Jar File.
+	 * 
+	 * @param fileJar
+	 *            The <File> Object of the Jar File.
+	 * @return Returns the <ClassLoader> for the Jar File.
+	 * @throws IOException
+	 */
 	private static ClassLoader loadJarClasses(File fileJar) throws IOException {
 		URL url = fileJar.toURI().toURL();
 		URL[] urls = { url };
@@ -529,5 +781,4 @@ public class Plugin extends Printable {
 		}
 		return loader;
 	}
-
 }
