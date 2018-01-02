657210121702314bf43d9864accf6d40d2503ec7
Jab <40BlocksUnder@gmail.com>
2018-01-02 02:31:48 -0600

    Documented ChatMessageEvent.

--- src/sledgehammer/event/ChatMessageEvent.java
+++ src/sledgehammer/event/ChatMessageEvent.java
@@ -23,13 +23,27 @@ import sledgehammer.lua.chat.ChatChannel;
 import sledgehammer.lua.chat.ChatMessage;
 import sledgehammer.module.chat.ModuleChat;
 
+/**
+ * Event passed that contains a <ChatMessage> with the <ChatChannel>.
+ * 
+ * @author Jab
+ */
 public class ChatMessageEvent extends Event {
 
+	/** The String ID of the Event. */
 	public static final String ID = "ChatMessageEvent";
 
+	/** The <ChatChannel> the <ChatMessage> is in. */
 	private ChatChannel chatChannel;
+	/** The invoked <ChatMessage>. */
 	private ChatMessage chatMessage;
 
+	/**
+	 * Main constructor.
+	 * 
+	 * @param chatMessage
+	 *            The <ChatMessage> invoked.
+	 */
 	public ChatMessageEvent(ChatMessage chatMessage) {
 		setMessage(chatMessage);
 		UUID channelId = getMessage().getChannelId();
@@ -37,30 +51,61 @@ public class ChatMessageEvent extends Event {
 		setChatChannel(moduleChat.getChatChannel(channelId));
 	}
 
+	/**
+	 * @return Returns the invoked <ChatMessage>.
+	 */
 	public ChatMessage getMessage() {
 		return this.chatMessage;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Sets the invoked <ChatMessage>.
+	 * 
+	 * @param chatMessage
+	 *            The <ChatMessage> to set.
+	 */
 	private void setMessage(ChatMessage chatMessage) {
 		this.chatMessage = chatMessage;
 	}
 
+	/**
+	 * @return Returns the <ChatChannel> that the invoked <ChatMessage> is in.
+	 */
 	public ChatChannel getChatChannel() {
 		return this.chatChannel;
 	}
 
+	/**
+	 * (Private Method)
+	 * 
+	 * Sets the <ChatChannel> that the invoked <ChatMessage> is in.
+	 * 
+	 * @param chatChannel
+	 *            The <ChatChannel> to set.
+	 */
 	private void setChatChannel(ChatChannel chatChannel) {
 		this.chatChannel = chatChannel;
 	}
 
+	/**
+	 * @return Returns the <ModuleChat> instance of the Sledgehammer engine.
+	 */
 	public ModuleChat getChatModule() {
 		return SledgeHammer.instance.getPluginManager().getChatModule();
 	}
 
+	/**
+	 * Returns the <String> Log message set for the Event.
+	 */
 	public String getLogMessage() {
 		return null;
 	}
 
+	/**
+	 * @return Returns the Event's ID.
+	 */
 	public String getID() {
 		return ID;
 	}
