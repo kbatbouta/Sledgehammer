8ca1e8f24f346a3197ab593e2630dce49f7920aa
Jab <40BlocksUnder@gmail.com>
2018-01-02 03:08:17 -0600

    Documented CoOpAccessEvent.

--- src/sledgehammer/event/CoOpAccessEvent.java
+++ src/sledgehammer/event/CoOpAccessEvent.java
@@ -16,43 +16,110 @@ This file is part of Sledgehammer.
  */
 package sledgehammer.event;
 
+/**
+ * CoOpEvent to handle Access requests to the PZ server.
+ * 
+ * @author Jab
+ */
 public class CoOpAccessEvent extends CoOpEvent {
 
+	/** The String ID of the Event. */
 	public static final String ID = "CoOpAccessEvent";
 
-	public static enum Result {
-		GRANTED, DENIED
-	}
-
+	/** The <Result> of the CopAccessEvent. */
 	private Result result = null;
-
+	/** The <String> reason provided for the <Result>. */
 	private String reason = null;
 
+	/**
+	 * Main constructor.
+	 * 
+	 * @param result
+	 *            The <Result> of the Co-Op Access Request.
+	 */
 	public CoOpAccessEvent(Result result) {
 		this.result = result;
 	}
 
+	/**
+	 * Alternative constructor for providing a <String> reason for the <Result>
+	 * given.
+	 * 
+	 * @param result
+	 *            The <Result> of the Co-Op Access Request.
+	 * @param reason
+	 *            The <String> reason for the <Result> given.
+	 */
 	public CoOpAccessEvent(Result result, String reason) {
 		this.result = result;
 		this.reason = reason;
 	}
 
-	public String getReason() {
-		return reason;
-	}
-
-	public Result getResult() {
-		return result;
-	}
-
 	@Override
 	public String getLogMessage() {
-		// TODO: LogEvent message;
 		return null;
 	}
 
 	@Override
 	public String getID() {
 		return ID;
+	}
+
+	/**
+	 * @return Returns the <String> reason for the <Result> given.
+	 */
+	public String getReason() {
+		return reason;
+	}
+
+	/**
+	 * @return Returns the <Result> of the Co-Op Access Request.
+	 */
+	public Result getResult() {
+		return result;
+	}
+
+	/**
+	 * Enumeration for CoOpAccessEvent results.
+	 * 
+	 * @author Jab
+	 */
+	public static enum Result {
+		// @formatter:off
+		GRANTED(0),
+		DENIED(1);
+		// @formatter:on
+
+		/** The <Integer> Id of the <Result>. */
+		private int id;
+
+		/**
+		 * Main constructor.
+		 * 
+		 * @param id
+		 *            The <Integer> id of the <Result>.
+		 */
+		private Result(int id) {
+			setId(id);
+		}
+
+		/**
+		 * @return Returns the <Integer> id of the <Result>.
+		 */
+		public int getId() {
+			return this.id;
+		}
+
+		/**
+		 * (Private Method)
+		 * 
+		 * Sets the <Integer> id of the <Result>.
+		 * 
+		 * @param id
+		 *            The <Integer> id to set.
+		 */
+		private void setId(int id) {
+			this.id = id;
+		}
 	}
 }
