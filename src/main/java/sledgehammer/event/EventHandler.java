a59364d392e73aba472ef24214aaac706ad12371
Jab <40BlocksUnder@gmail.com>
2018-01-23 01:08:06 -0600

    Documented EventHandler.

--- src/main/java/sledgehammer/event/EventHandler.java
+++ src/main/java/sledgehammer/event/EventHandler.java
@@ -18,29 +18,66 @@
  *    not affiliated with TheIndieStone, or it's immediate affiliates, or contractors.
  */
 
-package sledgehammer.manager;
+/*
+ * This file is part of Sledgehammer.
+ *
+ *    Sledgehammer is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Sledgehammer is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with Sledgehammer. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Sledgehammer is free to use and modify, ONLY for non-official third-party servers
+ *    not affiliated with TheIndieStone, or it's immediate affiliates, or contractors.
+ */
+
+package sledgehammer.event;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This Annotation handles the assignment and identification of a method to handle
+ * Events passed to the Sledgehammer engine.
+ *
+ * @author Jab
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface EventHandler {
+
+    /**
+     * Set this to true if the EventHandler should handle Events that are set
+     * cancelled prior to being handled by the EventHandler.
+     *
+     * @return Returns false by default.
+     */
     boolean ignoreCancelled() default false;
+
+    /**
+     * The EventHandler priority is a means of determining the importance of
+     * the EventHandler when compared to other handlers. If the number set is
+     * higher than another event handler, then the event handler will run
+     * first. The highest number is the most important.
+     *
+     * @return Returns 0 by default.
+     */
     int eventPriority() default 0;
+
+    /**
+     * The module ID defined is useful for determining the origin of the
+     * EventHandler, or the associated ID of the EventHandler. This is an
+     * optional setting.
+     * @return Returns an empty String by default.
+     */
     String moduleId() default "";
 }
-
-/*
-Object x, y; String s; int i;
-MethodType mt; MethodHandle mh;
-MethodHandles.Lookup lookup = MethodHandles.lookup();
-// mt is (char,char)String
-mt = MethodType.methodType(String.class, char.class, char.class);
-mh = lookup.findVirtual(String.class, "replace", mt);
-s = (String) mh.invokeExact("daddy",'d','n');
-// invokeExact(Ljava/lang/String;CC)Ljava/lang/String;
-assertEquals(s, "nanny");
- */
